973cfed04a325a8e429407f7c047a70287b0f610
pnamv <pnamvar22@gmail.com>
2025-09-29 02:25:52 -0700

    completed

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ House1.save()
 Person1 = Person("parisa","namvar","0150359136")
 Person1.save()
 
-Product1 = Product("Milk","35000","pegah")
+Product1 = Product("Milk","35000","pegah", "04-05-03")
 Product1.save()
 
 Lesson1 = Course("math", "mesbah", "3")
@@ -20,5 +20,5 @@ Lesson1.save()
 Sim1 = Sim("09386650502", "irancel", "parisa namvar")
 Sim1.save()
 
-Visit1 = Visit("mashhad", "amirian", "250000")
+Visit1 = Visit("mashhad", "amirian", "250000", "04-06-20")
 Visit1.save()
